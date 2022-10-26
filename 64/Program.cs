691b943c90bd1bb9232fda9b9d3f3763682f58f5
fosterjok <fosterjok@mail.ru>
2022-10-26 11:54:33 +0300

    64/66

---
+++ 64/Program.cs
@@ -0,0 +1,23 @@
+﻿/*int n ;
+Console.Write($"Введите число:  ");
+int.TryParse (Console.ReadLine(), out n);
+
+int Factorial (int _n)
+{
+    if (_n<=1)
+    return 1;
+    else return _n*Factorial(_n-1);
+}
+
+Console.Write($"{Factorial(n)}");*/
+
+int n ;
+Console.Write($"Введите число:  ");
+int.TryParse (Console.ReadLine(), out n);
+
+for (int i = 1; i <= n; i++)
+{   
+    
+    Console.Write($"{i} ");
+}
+
